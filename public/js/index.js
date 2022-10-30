cbbc6c397ec48728b1333da5dadd8a138e65d4da
Xifre Font <xifre.font.97@gmail.com>
2022-10-30 15:28:58 +0100

    Authors component skeleton

--- public/js/index.js
+++ public/js/index.js
@@ -3,11 +3,15 @@ import setCustomElements from "./utils/setCustomElements/setCustomElements.js";
 import { $ } from "./utils/selector/selector.js";
 const root = $("#root");
 setCustomElements(components);
-const { app, button } = components;
+const { app, button, newslist, newscontextprovider, authors } = components;
 root.innerHTML = `
   <${app.tag}>
       <${button.tag}>
         Hello
       </${button.tag}>
+      <${newscontextprovider.tag}>
+        <${newslist.tag}></${newslist.tag}>
+        <${authors.tag}></${authors.tag}>
+      </${newscontextprovider.tag}>
   </${app.tag}>
 `;
--- src/components/components.ts
+++ src/components/components.ts
@@ -1,13 +1,28 @@
+import Authors from "../features/news/components/Authors/Authors.js";
+import News from "../features/news/components/News/News.js";
+import NewsList from "../features/news/components/NewsList/NewsList.js";
+import NewsContextProvider from "../features/news/store/NewsContextProvider.js";
 import { Components } from "../types/components.js";
 import defineComponents from "../utils/defineComponents/defineComponents.js";
 import App from "./App/App.js";
 import Button from "./Button/Button.js";
 
-export const allComponents = [App, Button];
+export const allComponents = [
+  App,
+  Button,
+  News,
+  NewsList,
+  NewsContextProvider,
+  Authors,
+];
 
 export const basicComponents: Components = {
   app: {},
   button: {},
+  news: {},
+  newslist: {},
+  newscontextprovider: {},
+  authors: {},
 };
 
 export const components = defineComponents(allComponents, basicComponents);
